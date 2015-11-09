8a5aafa091970eeeda634a95aa320dc21941864f
Amber Fechko <amber@dendriticspine.com>
2015-11-09 15:57:43 -0800

    GPS conversion works! (.. now waiting for MS to fix location bug)

--- ConvertBandGPX.py
+++ ConvertBandGPX.py
@@ -17,7 +17,8 @@ import argparse
 import isodate
 import itertools as it
 
-bandDataFile = "GetActGPS.txt"
+bandDataFile = 'GetActGPS.txt'
+activityType = 'Running'
 
 # For the few oddball cases where there are missing key/pairs, fill in with properly formatted 0s
 class chkDict(dict):
@@ -49,64 +50,126 @@ rawData = re.sub(r'golfActivities', lambda x: 'golfActivities{{{}}}'.format(next
 # Load our data!
 data=json.loads(rawData, object_pairs_hook=chkDict)
 
+headerString = '<?xml version="1.0" encoding="UTF-8"?>\n<gpx\n\tversion="1.1"\n\tcreator="BandSandbox apps@dendriticspine.com"\n\t\
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n\txmlns="http://www.topografix.com/GPX/1/1"\n\t\
+xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"\n\t\
+xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1">\n<trk>\n'
+
+endingString = '</trkseg>\n</trk>\n</gpx>\n'
+
+gpsData = []
+activityStart = []
+activityStartStr = []
+speedData = []
+gpsTimeCalc = []
+mapPointType = []
+heartRatePoint = []
+pointLat = []
+pointLong = []
+pointEle = []
+
 # -------------------------------------------------
 # BICYCLING ACTIVITY DATA
 # -------------------------------------------------
-activityType = "Bicycling"
-gpsDataB = []
-activityStartB = []
-activityEndB = []
-speedDataB = []
-gpsTimeCalcB = []
-mapPointTypeB = []
-heartRatePointB = []
-pointLatB = []
-pointLongB = []
-pointEleB = []
+if (activityType == 'Cycling'):
+    # Pulling out relevant data from the JSON array 
+    for i1 in range(0,next(countFixBike)):
+        for i in range(0, len(data['bikeActivities{'+str(i1)+'}'])):
+            currStartTime = re.sub('.\d+[-+]\d\d:\d\d','',data['bikeActivities{'+str(i1)+'}'][i]['startTime'])
+            dtStartTime = dt.datetime.strptime(currStartTime, '%Y-%m-%dT%H:%M:%S')
+            activityStart.append(dtStartTime.strftime('%Y-%m-%dT%H:%M:%SZ'))
+            activityStartStr.append(dtStartTime.strftime('%Y-%m-%d-%H-%M-%S'))
+            for igps in range(0,len(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'])):
+                mapPointType.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['mapPointType'])
+                secSinceStart = data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['secondsSinceStart']
+                gpsTimeCalcTemp = dtStartTime + dt.timedelta(seconds=secSinceStart)
+                gpsTimeCalc.append(gpsTimeCalcTemp.strftime('%Y-%m-%dT%H:%M:%SZ'))
+                heartRatePoint.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['heartRate'])
+                speedData.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['speed'])
+                pointLat.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['latitude'])
+                pointLong.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['longitude'])
+                pointEle.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['elevationFromMeanSeaLevel'])
+            gpsData.append([pointLat, pointLong, pointEle, mapPointType, gpsTimeCalc, speedData, heartRatePoint])
+            speedData = []
+            gpsTimeCalc = []
+            mapPointType = []
+            heartRatePoint = []
+            pointLat = []
+            pointLong = []
+            pointEle = []
+    
+# -------------------------------------------------
+# RUNNING ACTIVITY DATA
+# -------------------------------------------------
+if (activityType == 'Running'):
+    # Pulling out relevant data from the JSON array 
+    for i1 in range(0,next(countFixRun)):
+        for i in range(0, len(data['runActivities{'+str(i1)+'}'])):
+            currStartTime = re.sub('.\d+[-+]\d\d:\d\d','',data['runActivities{'+str(i1)+'}'][i]['startTime'])
+            dtStartTime = dt.datetime.strptime(currStartTime, '%Y-%m-%dT%H:%M:%S')
+            activityStart.append(dtStartTime.strftime('%Y-%m-%dT%H:%M:%SZ'))
+            activityStartStr.append(dtStartTime.strftime('%Y-%m-%d-%H-%M-%S'))
+            for igps in range(0,len(data['runActivities{'+str(i1)+'}'][i]['mapPoints'])):
+                mapPointType.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['mapPointType'])
+                secSinceStart = data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['secondsSinceStart']
+                gpsTimeCalcTemp = dtStartTime + dt.timedelta(seconds=secSinceStart)
+                gpsTimeCalc.append(gpsTimeCalcTemp.strftime('%Y-%m-%dT%H:%M:%SZ'))
+                heartRatePoint.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['heartRate'])
+                speedData.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['speed'])
+                pointLat.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['latitude'])
+                pointLong.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['longitude'])
+                pointEle.append(data['runActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['elevationFromMeanSeaLevel'])
+            gpsData.append([pointLat, pointLong, pointEle, mapPointType, gpsTimeCalc, speedData, heartRatePoint])
+            speedData = []
+            gpsTimeCalc = []
+            mapPointType = []
+            heartRatePoint = []
+            pointLat = []
+            pointLong = []
+            pointEle = []
 
-# Pulling out relevant data from the JSON array 
-for i1 in range(0,next(countFixBike)):
-    for i in range(0, len(data['bikeActivities{'+str(i1)+'}'])):
-        activityStartB.append(re.sub('.\d+[-+]\d\d:\d\d','',data['bikeActivities{'+str(i1)+'}'][i]['startTime']))
-        currStartTime = re.sub('.\d+[-+]\d\d:\d\d','',data['bikeActivities{'+str(i1)+'}'][i]['startTime'])
-        dtStartTime = dt.datetime.strptime(currStartTime, '%Y-%m-%dT%H:%M:%S')
-        for igps in range(0,len(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'])):
-            mapPointTypeB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['mapPointType'])
-            secSinceStart = data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['secondsSinceStart']
-            gpsTimeCalcB.append((dtStartTime + dt.timedelta(seconds=secSinceStart)))
-            heartRatePointB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['heartRate'])
-            speedDataB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['speed'])
-            pointLatB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['latitude'])
-            pointLongB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['longitude'])
-            pointEleB.append(data['bikeActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['elevationFromMeanSeaLevel'])
-        activityEndB.append(re.sub('.\d+[-+]\d\d:\d\d','',data['bikeActivities{'+str(i1)+'}'][i]['endTime']))
-        gpsDataB.append([pointLatB, pointLongB, pointEleB, mapPointTypeB, gpsTimeCalcB, speedDataB, heartRatePointB])
-        speedDataB = []
-        gpsTimeCalcB = []
-        mapPointTypeB = []
-        heartRatePointB = []
-        pointLatB = []
-        pointLongB = []
-        pointEleB = []
-
-headerString = '<?xml version="1.0" encoding="UTF-8"?>\n<gpx\n\tversion="1.1"\n\tcreator="BandSandbox apps@dendriticspine.com"\n\t \
-                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n\txmlns="http://www.topografix.com/GPX/1/1"\n\t \
-                xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"\n\t \
-                xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1">\n<trk>\n'
-endingString = '</trkseg>\n</trk>\n</gpx>\n'
+# -------------------------------------------------
+# GOLF ACTIVITY DATA
+# -------------------------------------------------
+if (activityType == 'Golf'):
+    # Pulling out relevant data from the JSON array 
+    for i1 in range(0,next(countFixGolf)):
+        for i in range(0, len(data['golfActivities{'+str(i1)+'}'])):
+            currStartTime = re.sub('.\d+[-+]\d\d:\d\d','',data['golfActivities{'+str(i1)+'}'][i]['startTime'])
+            dtStartTime = dt.datetime.strptime(currStartTime, '%Y-%m-%dT%H:%M:%S')
+            activityStart.append(dtStartTime.strftime('%Y-%m-%dT%H:%M:%SZ'))
+            activityStartStr.append(dtStartTime.strftime('%Y-%m-%d-%H-%M-%S'))
+            for igps in range(0,len(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'])):
+                mapPointType.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['mapPointType'])
+                secSinceStart = data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['secondsSinceStart']
+                gpsTimeCalcTemp = dtStartTime + dt.timedelta(seconds=secSinceStart)
+                gpsTimeCalc.append(gpsTimeCalcTemp.strftime('%Y-%m-%dT%H:%M:%SZ'))
+                heartRatePoint.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['heartRate'])
+                speedData.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['speed'])
+                pointLat.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['latitude'])
+                pointLong.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['longitude'])
+                pointEle.append(data['golfActivities{'+str(i1)+'}'][i]['mapPoints'][igps]['location']['elevationFromMeanSeaLevel'])
+            gpsData.append([pointLat, pointLong, pointEle, mapPointType, gpsTimeCalc, speedData, heartRatePoint])
+            speedData = []
+            gpsTimeCalc = []
+            mapPointType = []
+            heartRatePoint = []
+            pointLat = []
+            pointLong = []
+            pointEle = []
+            
                     
-for p in range(0,len(activityStartB)):
-    fileNameString = activityType + '_' + activityStartB[p] + '.gpx'
+for p in range(0,len(activityStart)):
+    fileNameString = activityType + '_' + activityStartStr[p] + '.gpx'
     f = open(fileNameString, 'w')
     f.write(headerString)
 
-    nameTimeString = '<name><![CDATA[{0} {1}]]></name><time>{1}</time>\n<trkseg>'.format(activityType, activityStartB[p])
+    nameTimeString = '<name><![CDATA[{0} {1}]]></name><time>{1}</time>\n\n<trkseg>\n'.format(activityType, activityStart[p])
     f.write(nameTimeString)
     
-    for q in range(0,len(gpsDataB[i])):
-        gpsPointString = '<trkpt lat="{0}" lon="{1}"><ele>{2}</ele><name>{3}</name><time>{4}</time><speed>{5}</speed><desc>{6}</desc></trkpt>'.format(gpsDataB[p][q][0],gpsDataB[p][q][1],gpsDataB[p][q][2],gpsDataB[p][q][3],gpsDataB[p][q][4],gpsDataB[p][q][5],gpsDataB[p][q][6])
+    for q in range(0,len(gpsData[p][0])):
+        gpsPointString = '<trkpt lat="{0}" lon="{1}"><ele>{2}</ele><name>{3}</name><time>{4}</time><speed>{5}</speed><desc>{6}</desc></trkpt>\n'.format(gpsData[p][0][q],gpsData[p][1][q],gpsData[p][2][q],gpsData[p][3][q],gpsData[p][4][q],gpsData[p][5][q],gpsData[p][6][q])
         f.write(gpsPointString)
         
     f.write(endingString)
     f.close()
-    
